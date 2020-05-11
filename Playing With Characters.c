ee05b9662b17e1718b0f1443c2d106e31720f287
Divya Kelaskar <56636487+divyakelaskar@users.noreply.github.com>
2020-05-11 16:28:29 +0530

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+    int size;
+    char c;
+    scanf("%c", &c);
+    char s[8];
+    scanf("%s", &s);
+    size=strlen(s);
+    s[8]=s[size];
+    char s1[14];
+    scanf(" %[^\t\n]s", &s1);
+
+    printf("%c\n", c);
+    for(int i=0;i<size;i++)
+    {
+    printf("%c", s[i]);
+    }
+    printf("\n");
+    printf("%s", s1);
+    return 0;
+} 
